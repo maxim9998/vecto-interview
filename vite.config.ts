4c804937d904a748ed349ff3e50527bfe5fbfa64
maxim9998 <maks.harutyunyan@gmail.com>
2025-08-07 15:38:27 +0400

    initial config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,47 @@
-import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+import { defineConfig } from 'vite'
+import svgr from 'vite-plugin-svgr'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss(), tsconfigPaths(), svgr()],
+  server: {
+    open: true,
+    port: 7000
+  },
+  base: '/',
+   build: {
+    target: 'esnext',
+    sourcemap: false,
+    minify: 'esbuild',
+    outDir: 'dist',
+    chunkSizeWarningLimit: 400,
+
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          lodash: ['lodash'],
+        },
+      },
+    },
+  },
+   optimizeDeps: {
+    esbuildOptions: {
+      define: {
+        global: 'globalThis',
+      },
+    },
+  },
+
+   resolve: {
+    alias: {
+      '@app': path.resolve(__dirname, 'src/app'),
+      '@assets': path.resolve(__dirname, 'src/assets'),
+      '@features': path.resolve(__dirname, 'src/features'),
+      '@shared': path.resolve(__dirname, 'src/shared'),
+    },
+  },
 })
